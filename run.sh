cf48d7886654a9af69060f002f028d98b9d1daee
taivop <taivo.pungas@gmail.com>
2015-08-15 10:13:29 +0300

    mine in 4 batches of 10 parallel jobs each

--- run.sh
+++ run.sh
@@ -1,6 +1,48 @@
-id_start=2500000
-id_end=$((2600000-1))
-step=10000
+id_start=2350000
+id_end=$((2500000-1))
+step=15000
+
+for i in `seq -f %.0f $id_start $step $id_end`
+do
+	r < scrape.R --no-save --args $i $((i+step-1)) &
+
+done
+
+
+wait
+
+
+id_start=2200000
+id_end=$((2350000-1))
+step=15000
+
+for i in `seq -f %.0f $id_start $step $id_end`
+do
+	r < scrape.R --no-save --args $i $((i+step-1)) &
+
+done
+
+
+wait
+
+
+id_start=2050000
+id_end=$((2200000-1))
+step=15000
+
+for i in `seq -f %.0f $id_start $step $id_end`
+do
+	r < scrape.R --no-save --args $i $((i+step-1)) &
+
+done
+
+
+wait
+
+
+id_start=1900000
+id_end=$((2050000-1))
+step=15000
 
 for i in `seq -f %.0f $id_start $step $id_end`
 do

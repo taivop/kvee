90ce7bc6fb41d07aa921afe056c0f37b1a622546
taivop <taivo.pungas@gmail.com>
2015-08-18 01:49:09 +0300

    add script to look up KV index values

---
+++ load_index.R
@@ -0,0 +1,17 @@
+library(readxl)
+library(dplyr)
+
+indeks <- read_excel("indeks/kvindeks.xlsx") %>%
+  select(Kuupäev, Indeks) %>%
+  mutate(Kuupäev=as.Date(Kuupäev, "%d.%m.%y"),
+         Indeks=as.numeric(Indeks))
+
+indeks_lookup <- indeks
+
+# For each index data point, create 6 more dates for lookup
+for(shift in 1:6) {
+  shifted <- indeks %>%
+    mutate(Kuupäev=Kuupäev+shift)
+  
+  indeks_lookup <- rbind(indeks_lookup, shifted)
+}

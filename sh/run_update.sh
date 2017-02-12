0c0c0128c128022dc379a3f70a45003fa7daa947
taivop <taivo.pungas@gmail.com>
2017-02-12 12:47:34 +0100

    run script

--- sh/run_update.sh
+++ sh/run_update.sh
@@ -5,8 +5,8 @@ step=$(((id_end-id_start+num_threads)/num_threads))
 
 for i in `seq -f %.0f $id_start $step $id_end`
 do
-	#R < scrape.R --no-save --args $i $((i+step-1)) &
-	echo $i $((i+step-1))
+	R < scrape.R --no-save --args $i $((i+step-1)) &
+	#echo $i $((i+step-1))
 done
 
 

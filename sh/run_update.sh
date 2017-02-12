22f16095856dd53023837544d7be2bb8214fa812
taivop <taivo.pungas@gmail.com>
2017-02-12 12:02:04 +0100

    bugfix

--- sh/run_update.sh
+++ sh/run_update.sh
@@ -1,10 +1,10 @@
 id_start=2747175
 id_end=$((2887616))
-step=25000
+step=36000
 
 for i in `seq -f %.0f $id_start $step $id_end`
 do
-	r < scrape.R --no-save --args $i $((i+step-1)) &
+	R < scrape.R --no-save --args $i $((i+step-1)) &
 
 done
 
--- sh/setup.sh
+++ sh/setup.sh
@@ -1,9 +1,15 @@
 sudo apt-get update
-sudo apt-get install r-base r-base-dev
 sudo apt-get install libopenblas-base 
 sudo apt-get install littler
+sudo apt-get install libssl-dev libcurl4-openssl-dev libxml2-dev
 
-r r/install_dependencies.r
+# Add CRAN source
+sudo sh -c 'echo "deb http://cran.rstudio.com/bin/linux/ubuntu trusty/" >> /etc/apt/sources.list'
+gpg --keyserver keyserver.ubuntu.com --recv-key E084DAB9
+gpg -a --export E084DAB9 | sudo apt-key add -
+
+sudo apt-get -y install r-base r-base-dev
+sudo R --no-save < r/install_dependencies.R
 
 mkdir data_out
 mkdir logs

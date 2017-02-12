511973442cab8999e8d7ae792c93dc0315c3f5a6
taivop <taivo.pungas@gmail.com>
2017-02-12 12:45:04 +0100

    more threads

--- sh/run_update.sh
+++ sh/run_update.sh
@@ -1,11 +1,12 @@
 id_start=2747175
 id_end=$((2887616))
-step=36000
+num_threads=8
+step=$(((id_end-id_start+num_threads)/num_threads))
 
 for i in `seq -f %.0f $id_start $step $id_end`
 do
-	R < scrape.R --no-save --args $i $((i+step-1)) &
-
+	#R < scrape.R --no-save --args $i $((i+step-1)) &
+	echo $i $((i+step-1))
 done
 
 
